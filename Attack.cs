c51d5b723078720d5302b6405baad7cd07a7b940
Steed Huang <steedhuang2002@gmail.com>
2023-12-30 02:13:33 -0500

    bullet damages enemy

--- Attack.cs
+++ Attack.cs
@@ -1,4 +1,4 @@
-using System.Numerics;
+using Godot;
 
 public class Attack
 {
--- Bullet.cs
+++ Bullet.cs
@@ -6,6 +6,9 @@ public partial class Bullet : CharacterBody2D
   public const float Speed = 500.0f;
   private Vector2 direction = new Vector2();
 
+  private const float attackDamage = 50.0f;
+  private const float knockbackForce = 100.0f;
+
   public override void _Ready()
   {
     direction = new Vector2(1, 0).Rotated(Rotation);
@@ -18,8 +21,26 @@ public partial class Bullet : CharacterBody2D
 
     KinematicCollision2D collisionResult = MoveAndCollide(Velocity * (float)delta);
 
+    // tile collisions
     if (collisionResult is not null)
     {
+      QueueFree();
+    }
+  }
+
+  private void _on_area_2d_area_entered(Area2D area)
+  {
+    GD.Print("test");
+    if (area is HitboxComponent component)
+    {
+      Attack a = new Attack
+      {
+        damage = attackDamage,
+        knockbackForce = knockbackForce,
+        position = GlobalPosition
+      };
+
+      component.Damage(a);
       QueueFree();
     }
   }
